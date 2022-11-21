63a24f0f19f7e290dae3499bfc0fe161137bfa9d
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-11-21 13:01:24 -0300

    added @ComponentScan to avoid error on AppUserService not being recognized

--- src/main/java/com/mmacedoaraujo/logindemo/LoginDemoApplication.java
+++ src/main/java/com/mmacedoaraujo/logindemo/LoginDemoApplication.java
@@ -2,8 +2,12 @@ package com.mmacedoaraujo.logindemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@ComponentScan("com.mmacedoaraujo.logindemo.appuser.AppUserService")
 public class LoginDemoApplication {
 
 	public static void main(String[] args) {
